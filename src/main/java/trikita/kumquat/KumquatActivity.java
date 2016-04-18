4cea672ed1d824ed10a4c577c9a14339e2c586e3
Anna Kruglik <kruglik.anna@gmail.com>
2016-04-18 12:04:57 +0800

    initial commit

---
+++ src/main/java/trikita/kumquat/KumquatActivity.java
@@ -0,0 +1,12 @@
+package trikita.kumquat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KumquatActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
