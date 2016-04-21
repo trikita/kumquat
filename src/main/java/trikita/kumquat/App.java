59aa327c540c9f05e3b465589f4a51d77a46950a
Anna Kruglik <kruglik.anna@gmail.com>
2016-04-21 21:34:47 +0800

    tied together state, reducer and mqtt middleware

---
+++ src/main/java/trikita/kumquat/App.java
@@ -0,0 +1,37 @@
+package trikita.kumquat;
+
+import android.app.Application;
+
+import trikita.anvil.Anvil;
+import trikita.jedux.Action;
+import trikita.jedux.Store;
+
+public class App extends Application {
+
+    private static App sInstance;
+
+    private Store<Action, State> store;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+
+        State initialState = State.getDefault();
+        MqttController mqtt = new MqttController(this, initialState.connections());
+
+        this.store = new Store<>(new State.Reducer(), initialState, mqtt);
+
+        this.store.subscribe(Anvil::render);
+    }
+
+    public static State state() {
+        return sInstance.store.getState();
+    }
+
+    public static State dispatch(Action action) {
+        return sInstance.store.dispatch(action);
+    }
+}
+
+
