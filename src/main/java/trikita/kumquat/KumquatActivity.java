4b453ba58fb5d7e5a24e9214b48714c30a8d23d3
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2016-05-07 12:05:15 +0800

    added drawer layout

--- src/main/java/trikita/kumquat/KumquatActivity.java
+++ src/main/java/trikita/kumquat/KumquatActivity.java
@@ -1,13 +1,101 @@
 package trikita.kumquat;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.Arrays;
+
+import trikita.anvil.Anvil;
+import trikita.anvil.RenderableAdapter;
+import trikita.anvil.RenderableView;
+
+import static trikita.anvil.DSL.*;
+import static trikita.anvil.support.v4.Supportv4DSL.*;
 
 public class KumquatActivity extends AppCompatActivity {
+
+    private NavigationScreen navigationScreen;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new ListLayout(this));
+        navigationScreen = new NavigationScreen(this);
+        setContentView(navigationScreen);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return navigationScreen.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("ViewConstructor")
+    private static class NavigationScreen extends RenderableView {
+
+        private final AppCompatActivity activity;
+        private DrawerLayout drawer;
+        private ActionBarDrawerToggle drawerToggle;
+
+        private String[] nav = {"Connections", "Cards", "Favourites", "Settings"};
+        private RenderableAdapter navAdapter = RenderableAdapter.withItems(Arrays.asList(nav), this::navigationItemView);
+
+        public NavigationScreen(AppCompatActivity activity) {
+            super(activity);
+            this.activity = activity;
+        }
+
+        @Override
+        public void view() {
+            drawerLayout(() -> {
+                init(() -> {
+                    drawer = Anvil.currentView();
+                    drawerToggle = new ActionBarDrawerToggle(activity, drawer, R.string.drawer_open, R.string.drawer_close) {
+                        public void onDrawerClosed(View view) {
+                            super.onDrawerClosed(view);
+                        }
+                        public void onDrawerOpened(View drawerView) {
+                            super.onDrawerOpened(drawerView);
+                        }
+                    };
+                    drawer.addDrawerListener(drawerToggle);
+                    activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                    activity.getSupportActionBar().setHomeButtonEnabled(true);
+                    drawerToggle.syncState();
+                });
+                size(FILL, FILL);
+                frameLayout(() -> {
+                    size(FILL, FILL);
+                    textView(() -> {
+                        text("Content");
+                    });
+                });
+                listView(() -> {
+                    size(dip(240), FILL);
+                    layoutGravity(START);
+                    adapter(navAdapter);
+                    backgroundColor(Color.WHITE);
+                    onItemClick((av, v, pos, id) -> {
+                        // Handle click here
+                        drawer.closeDrawer(av);
+                    });
+                });
+            });
+        }
+
+        private void navigationItemView(int index, String item) {
+            textView(() -> {
+                size(FILL, dip(48));
+                text(item);
+            });
+        }
+
+        public boolean onOptionsItemSelected(MenuItem item) {
+            return drawerToggle.onOptionsItemSelected(item);
+        }
     }
 }
